jbeich/platform_system_extras

Fix misc-macro-parentheses warnings in f2fs_utils.
Bug: 28705665
Change-Id: I3341451eb77dd2539d70f3d3fa14f7eca188a63b

f2fs_utils/f2fs_sparseblock.c
@@ -26,9 +26,9 @@
       #member, le64_to_cpu((ptr)->member), le64_to_cpu((ptr)->member) );  \
   } while (0);
 
-#define segno_in_journal(sum, i)    (sum->sit_j.entries[i].segno)
+#define segno_in_journal(sum, i)    ((sum)->sit_j.entries[i].segno)
 
-#define sit_in_journal(sum, i)      (sum->sit_j.entries[i].se)
+#define sit_in_journal(sum, i)      ((sum)->sit_j.entries[i].se)
 
 static void dbg_print_raw_sb_info(struct f2fs_super_block *sb)
 {

Flash-ROM/system_extras

f2fs_utils/f2fs_sparseblock.c
@@ -26,9 +26,9 @@
       #member, le64_to_cpu((ptr)->member), le64_to_cpu((ptr)->member) );  \
   } while (0);
 
-#define segno_in_journal(jnl, i)    (jnl->sit_j.entries[i].segno)
+#define segno_in_journal(jnl, i)    ((jnl)->sit_j.entries[i].segno)
 
-#define sit_in_journal(jnl, i)      (jnl->sit_j.entries[i].se)
+#define sit_in_journal(jnl, i)      ((jnl)->sit_j.entries[i].se)
 
 static void dbg_print_raw_sb_info(struct f2fs_super_block *sb)
 {